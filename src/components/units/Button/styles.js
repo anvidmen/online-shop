d34ba9ed6ba2ddea26cd9787b1322208e864c624
Juan David Om <anvidmen@gmail.com>
2021-02-03 14:52:27 +0100

    create units components

---
+++ src/components/units/Button/styles.js
@@ -0,0 +1,21 @@
+import styled from 'styled-components'
+
+export const StyledButton = styled.button`
+    border: none;
+    border-radius: 6px;
+    opacity: 1;
+    cursor: pointer;
+    font: normal normal normal 20px/25px Helvetica Neue;
+    width: 100%;
+    height: 2.5rem;
+    margin: 1rem;
+    text-align: center;
+    outline: none;
+    
+    &.styleButton {
+        background: transparent
+        linear-gradient(98deg, #e3108f 100%, #a60867 100%) 0% 0% no-repeat;
+        padding-box;
+        color: white;
+    };
+`
