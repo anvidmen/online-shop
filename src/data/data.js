3c5df12fe22678f479c8741383d400d8ec4c120c
Juan David Om <anvidmen@gmail.com>
2021-02-05 12:32:58 +0100

    modify data

---
+++ src/data/data.js
@@ -0,0 +1,10 @@
+import { Coffee, GreenTea, Strawberries } from 'images/index'
+
+const data = {
+  products: [
+    { id: 'GR1', name: 'Green tea', price: 3.11, img: GreenTea },
+    { id: 'SR1', name: 'Strawberries', price: 5.00, img: Strawberries },
+    { id: 'CF1', name: 'Coffee', price: 11.23, img: Coffee }
+  ]
+}
+export default data
