7a4a2f514fe4a11ea462209e46ff21321811b649
Juan David Om <anvidmen@gmail.com>
2021-02-05 12:35:40 +0100

    modify routes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,40 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Route, Switch } from 'react-router-dom'
 import Home from 'screens/Home/Home'
+import MyCart from 'screens/UserFlow/MyCart/MyCart'
+import CartDropdown from 'screens/UserFlow/CartDropdown/CartDropdown'
+import data from 'data/data'
 
 const App = () => {
+  const [cartItems, setCartItems] = useState([])
+  const { products } = data
+
+  const addToCart = (product) => {
+    const exist = cartItems.find((item) => item.id === product.id)
+
+    if (exist) {
+      setCartItems(cartItems.map((item) =>
+        item.id === product.id ? { ...exist, qty: exist.qty + 1 } : item
+      ))
+    } else setCartItems([...cartItems, { ...product, qty: 1 }])
+  }
+
+  const handleDeleteCart = () => {
+    setCartItems([])
+  }
+
   return (
     <>
       <Switch>
-        <Route exact path='/' component={Home} />
+        <Route exact path='/'>
+          <Home products={products} addToCart={addToCart} />
+        </Route>
+        <Route exact path='/Checkout'>
+          <MyCart cartItems={cartItems} addToCart={addToCart} removeCart={handleDeleteCart} />
+        </Route>
+        <Route exact path='/cart'>
+          <CartDropdown cartItems={cartItems} removeCart={handleDeleteCart} />
+        </Route>
       </Switch>
     </>
   )
