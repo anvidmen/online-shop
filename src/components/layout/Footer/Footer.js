94c75c07dca584340a2268e2c3886b9564691742
Juan David Om <anvidmen@gmail.com>
2021-02-09 22:25:16 +0100

    fix footer

--- src/components/layout/Footer/Footer.js
+++ src/components/layout/Footer/Footer.js
@@ -1,9 +1,34 @@
 import React from 'react'
-import { StyledFooter } from './styles'
+import { Avatar, Link } from '@material-ui/core'
+import LinkedInIcon from '@material-ui/icons/LinkedIn'
+import GitHubIcon from '@material-ui/icons/GitHub'
+import TwitterIcon from '@material-ui/icons/Twitter'
+import { Container, Information, Row, StyledFooter } from './styles'
+
 const Footer = () => {
   return (
     <StyledFooter>
-      soy el footer
+      <Container>
+        <Information>
+          <div>
+            <h2>Shopping</h2>
+            <Information>
+              <p>Small virtual store where you will find great offers of products.</p>
+            </Information>
+          </div>
+          <div>
+            <h2>Contact</h2>
+            <Information>
+              <Link href='https://www.linkedin.com/in/juan-om'><Avatar><LinkedInIcon /></Avatar></Link>
+              <Link href='https://github.com/anvidmen'><Avatar><GitHubIcon /></Avatar></Link>
+              <Link href='https://twitter.com/JuanDaOm'><Avatar><TwitterIcon /></Avatar></Link>
+            </Information>
+          </div>
+        </Information>
+        <Row>
+          Copyright © <Link href='https://anvidmen.netlify.app/'>Anvidmen</Link>{new Date().getFullYear()} | All Rights Reserved
+        </Row>
+      </Container>
     </StyledFooter>
   )
 }
