15eb868bcce3ab54ae902cb3b758e463a290e81a
Juan David Om <anvidmen@gmail.com>
2021-02-10 21:11:18 +0100

    create screen Order

---
+++ src/screens/UserFlow/Order/Order.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import { StylesOrder } from './styles.js'
+import Body from 'components/layout/Body/Body'
+
+const Order = () => {
+  return (
+    <Body title='Order'>
+      <StylesOrder>
+        <h1>Thanks for your purchase, see you next time...</h1>
+        <img alt='' src='https://2.bp.blogspot.com/-eZrZjiuwG9k/VuXcZotoVBI/AAAAAAAAAMU/m1iVuT_i1v8XzF1k3FYLBGSS51i5HruxA/s1600/swingmation-wip.gif' />
+      </StylesOrder>
+    </Body>
+  )
+}
+
+export default Order
