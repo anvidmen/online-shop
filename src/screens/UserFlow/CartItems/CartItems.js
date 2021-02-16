260d346080290f1ac463f5ea2c201f31451256d2
Juan David Om <anvidmen@gmail.com>
2021-02-16 23:34:13 +0100

    add logic discount products

--- src/screens/UserFlow/CartItems/CartItems.js
+++ src/screens/UserFlow/CartItems/CartItems.js
@@ -2,35 +2,10 @@ import React from 'react'
 import { Cart, Container, Item, Message, Price, Total, Wrapper } from './styles'
 
 const CartItems = ({ cartItems }) => {
-  const calculateDiscountCoffee = () => {
-    let discount = 0
-    const discCoffee = 2 / 3
-    const discStrawberries = 0.50
-    const discGreenTea = 2
-
-    for (let i = 0; i < cartItems.length; i++) {
-      const item = cartItems[i]
-
-      if (item.name === 'Coffee' && item.qty > 2) discount = item.price * item.qty * discCoffee
-      else discount = item.price * item.qty
-
-      // if (item.name === 'Strawberries' && item.qty > 2) discount = (item.price * item.qty) - (discStrawberries * item.qty)
-      // else discount = item.price * item.qty
-
-      // if (item.name === 'Green tea' && item.qty === 2) discount = (item.price * item.qty) / discGreenTea
-      // else if (item.qty > 2) discount = (item.price * item.qty) - item.price
-    }
-    return discount
-  }
-
   const calculateTotalPrice = cartItems => {
-    let totalItemsPrice = 0
-
-    for (const productSelection of cartItems) {
-      const itemPrice = productSelection.price * productSelection.qty
-      totalItemsPrice += itemPrice
-    }
-    return totalItemsPrice
+    return cartItems.reduce((totalItemsPrice, currentItem) =>
+      totalItemsPrice + currentItem.totalPrice
+    , 0)
   }
 
   return (
@@ -50,8 +25,8 @@ const CartItems = ({ cartItems }) => {
             <Item>{item.name}</Item>
             <Item>{item.qty}</Item>
             <Item>€ {item.price}</Item>
-            <Item>€ {(item.price * item.qty - calculateDiscountCoffee(cartItems)).toFixed(2)}</Item>
-            <Price>€ {calculateDiscountCoffee(cartItems).toFixed(2)}</Price>
+            <Item>€ {item.discount.toFixed(2)}</Item>
+            <Price>€ {item.totalPrice.toFixed(2)}</Price>
           </Container>))) : (<Message>Your cart is empty</Message>)}
       </Wrapper>
       <Total>
